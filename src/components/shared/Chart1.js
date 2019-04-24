b0fdc9688b056dcedcabf4e10b7bf9a7f15cc660
adam mckenzie <adammckenzie@adams-MacBook-Pro-2.local>
2019-04-24 15:38:43 -0500

    initial

--- src/components/shared/Chart1.js
+++ src/components/shared/Chart1.js
@@ -1,37 +1,39 @@
-import React, { Component } from 'react';
-import { Doughnut } from 'react-chartjs-2';
-
+import React, { Component } from "react";
+import { Doughnut } from "react-chartjs-2";
 
 class Chart1 extends Component {
   render() {
+    console.log(this.props.purchases);
     let { purchases, budgetLimit } = this.props;
     purchases = purchases || [];
     const moneySpent = purchases.reduce((total, purchase) => {
-      return total + purchase.price
-    }, 0)
-    const remainingBudget = (budgetLimit - moneySpent) >= 0 ? (budgetLimit - moneySpent) : 0;
+      return total + purchase.price;
+    }, 0);
+    const remainingBudget =
+      budgetLimit - moneySpent >= 0 ? budgetLimit - moneySpent : 0;
     return (
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center"
+        }}
+      >
         <h6>Limit: ${budgetLimit}</h6>
-        <Doughnut data={{
-          labels: [
-            'Spent',
-            'Remainder'
-          ],
-          datasets: [{
-            data: [moneySpent, remainingBudget],
-            backgroundColor: [
-              '#C82233',
-              '#72C934',
-            ],
-            hoverBackgroundColor: [
-              '#C82233',
-              '#72C934',
+        <Doughnut
+          data={{
+            labels: ["Spent", "Remainder"],
+            datasets: [
+              {
+                data: [moneySpent, remainingBudget],
+                backgroundColor: ["#C82233", "#72C934"],
+                hoverBackgroundColor: ["#C82233", "#72C934"]
+              }
             ]
-          }]
-        }} />
+          }}
+        />
       </div>
-    )
+    );
   }
 }
 
